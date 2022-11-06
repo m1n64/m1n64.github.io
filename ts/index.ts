e39576c0c76b65b78392383d5b2c52be04880016
Kirill Sakharov <mrcaxapov@gmail.com>
2022-11-06 03:12:58 +0300

    Add AOS (why? IDK)

--- ts/index.ts
+++ ts/index.ts
@@ -1,5 +1,7 @@
 import Alpine from "alpinejs";
 import ScrollReveal from "scrollreveal";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 Alpine.data("indexPage", ()=>({
     loading: <boolean>true,
@@ -8,9 +10,16 @@ Alpine.data("indexPage", ()=>({
         ScrollReveal().reveal(".section");
         ScrollReveal().reveal(".section-block");
         ScrollReveal().reveal(".multilines-block");
+
+        AOS.init({
+            useClassNames: true,
+            initClassName: "section",
+            animatedClassName: "animate__fadeInLeft"
+        });
+
         setTimeout(()=>{
             this.loading = !this.loading;
-        }, 600)
+        }, 300)
     }
 }));
 
