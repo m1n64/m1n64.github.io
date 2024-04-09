b0eddede336b654ee5efe8549d3365114573f36b
m1n64 <mrcaxapov@gmail.com>
2024-04-09 14:55:07 +0300

    add og image

--- src/Components/Head/Head.tsx
+++ src/Components/Head/Head.tsx
@@ -3,7 +3,7 @@ import { Helmet } from 'react-helmet';
 // @ts-ignore
 import favicon from '../../assets/favico.ico';
 // @ts-ignore
-import pngIcon from '../../assets/favico.png';
+import metaImage from '../../assets/meta-bg.png';
 
 type HeadProps = {
     language?: string
@@ -15,7 +15,6 @@ export const Head: React.FC<HeadProps> = ({language = "en"}) => {
         description: "CV of Kirill Sakharov - Fullstack Web Developer",
         keywords: "cv, php, laravel, yii, go, golang, react, next.js, typescript",
         author: "Kirill Sakharov",
-        image: pngIcon
     };
 
     if (language === "ru") {
@@ -23,7 +22,6 @@ export const Head: React.FC<HeadProps> = ({language = "en"}) => {
         meta.description = "Резюме Кирилла Сахарова - Fullstack Web Developer";
         meta.keywords = "резюме, php, laravel, yii, go, golang, react, next.js, typescript";
         meta.author = "Кирилл Сахаров";
-        meta.image = pngIcon
     }
 
     return (
@@ -32,16 +30,17 @@ export const Head: React.FC<HeadProps> = ({language = "en"}) => {
                 <title>{meta.name}</title>
                 <link rel="icon" href={favicon} type="image/x-icon" />
                 <meta name="description" content={meta.description} />
-                <meta name="keywords" content={meta.description} />
+                <meta name="keywords" content={meta.keywords} />
                 <meta name="author" content={meta.author} />
                 <meta property="og:title" content={meta.name} />
+                <meta property="og:site_name" content="Kirill Sakharov CV" />
                 <meta property="og:description" content={meta.description} />
-                <meta property="og:image" content={meta.image} />
+                <meta property="og:image" content={metaImage} />
                 <meta property="og:url" content="https://m1n64.github.io/" />
                 <meta property="og:type" content="profile" />
                 <meta name="X:title" content={meta.name} />
                 <meta name="X:description" content={meta.description} />
-                <meta name="X:image" content={meta.image} />
+                <meta name="X:image" content={metaImage} />
                 <meta name="X:card" content="summary_large_image" />
                 <meta name="robots" content="all" />
             </Helmet>
